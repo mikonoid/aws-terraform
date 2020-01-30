9c7367d57c699b1428ac2ac4871f446907430d77
Mike Ivanov <mikonoid@gmail.com>
2020-01-30 19:22:03 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
+#sensitive data! don't share real s3 bucket to github
 terraform {
   backend "s3" {
-    bucket = "states"
+    bucket = "states-mike-test"
     key = "states/terraform.tfstate"
     region = "eu-central-1"
   }
