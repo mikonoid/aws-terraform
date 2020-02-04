290084ef5bb7f10405628c9f17c6f021141b0391
mivanovvs <mivanov@edgegravity.ericsson.com>
2020-02-04 15:22:30 +0200

    update infrastructure, migrate to sqlite

--- variables.tf
+++ variables.tf
@@ -2,9 +2,6 @@ variable "aws_region" {}
 variable "aws_profile" {}
 variable "localip" {}
 variable "key_name" {}
-variable "db_instance_class" {}
-variable "dbname" {}
-variable "dbuser" {}
 variable "public_key_path" {}
 variable "dev_instance_type" {}
 variable "dev_ami" {}
